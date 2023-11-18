9c07c689f2c56c280dc08c162dd9f422dd6da962
aryan <aryan.aryal86@gmail.com>
2023-11-18 13:04:58 -0700

    fix bot

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
+import os
 import discord
-import dotenv
-import random
-from discord.ext import commands
-bot = commands.Bot(command_prefix='/')
-@bot.event
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('token')
+
+intents = discord.Intents(8)
+client = discord.Client(intents=intents)
+
+@client.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name} ({bot.user.id})')
-    print('------')
-@bot.command(name = 'info')
-async def info(ctx):
-    await ctx.send('ShawarmaSheriff is a gacha game where you randomly draw donairs of different rarities')
-bot.run()
+    print(f'{client.user}) has connected to Discord!')
+
+client.run('token')
