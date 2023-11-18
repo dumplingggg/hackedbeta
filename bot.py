34175c75b75c752249e6b918554a1fcbf6208f4f
aryan <aryan.aryal86@gmail.com>
2023-11-18 13:48:39 -0700

    bot worky

--- bot.py
+++ bot.py
@@ -1,15 +1,25 @@
 import os
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('token')
 
-intents = discord.Intents(8)
-client = discord.Client(intents=intents)
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user}) has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
-client.run('token')
+@bot.command(name='serverinfo')
+async def server_info(ctx):
+    guild = ctx.guild
+    embed = discord.Embed(title="Server Information", color=0x00ff00)
+    embed.add_field(name="Server Name", value=guild.name, inline=False)
+    embed.add_field(name="Server ID", value=guild.id, inline=False)
+    embed.add_field(name="Members", value=guild.member_count, inline=False)
+    await ctx.send(embed=embed)
+
+bot.run(TOKEN)
