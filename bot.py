7f946bb1035422ac22166d478c7b12b740b611c0
aryan <aryan.aryal86@gmail.com>
2023-11-18 13:54:22 -0700

    fix bot

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('token')
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.command(name='serverinfo')
+async def server_info(ctx):
+    guild = ctx.guild
+    embed = discord.Embed(title="Server Information", color=0x00ff00)
+    embed.add_field(name="Server Name", value=guild.name, inline=False)
+    embed.add_field(name="Server ID", value=guild.id, inline=False)
+    embed.add_field(name="Members", value=guild.member_count, inline=False)
+    await ctx.send(embed=embed)
+
+bot.run(TOKEN)
